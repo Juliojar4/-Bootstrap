2cb974309f20db566a24d08bc68c539e76a390b7
julio jara <juliojaraitape@hotmail.com>
2022-05-05 22:20:56 -0300

    -Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Container</h1>
+      <div className='container-md border bg-dark text-white'>
+        <p>Este contaniner é fixedEste contaniner é fixedEste contaniner é 
+          fixedEste contaniner é fixedEste contaniner é fixedEste contaniner 
+          é fixedEste contaniner é fixedEste contaniner é fixedEste 
+          contaniner é fixedEste contaniner é fixedEste contaniner é fixedEste 
+          contaniner é fixedEste contaniner é fixedEste contaniner é fixedEste c
+          ontaniner é fixedEste contaniner é fixedEste d</p>
+      </div>
+      <div className='container-sm border bg-primary'>
+        <p>Este contaniner é fixedEste contaniner é fixedEste contaniner é 
+          fixedEste contaniner é fixedEste contaniner é fixedEste contaniner 
+          é fixedEste contaniner é fixedEste contaniner é fixedEste 
+          contaniner é fixedEste contaniner é fixedEste contaniner é fixedEste 
+          contaniner é fixedEste contaniner é fixedEste contaniner é fixedEste c
+          ontaniner é fixedEste contaniner é fixedEste d</p>
+      </div>
+      <h1>Grid</h1>
+      <div className='container-fluid border border-primary p-5'>
+
+        <div className='row'>
+          <div className='col border'>Coluna 1</div>
+          <div className='col border'>Coluna 2</div>
+          <div className='col border'>Coluna 3</div>
+        </div>
+
+        <div className='row'>
+          <div className='col border'>Coluna 1</div>
+          <div className='col-3 border'>Coluna 2</div>
+          <div className='col border'>Coluna 3</div>
+        </div>
+
+      </div>
     </div>
   );
 }
